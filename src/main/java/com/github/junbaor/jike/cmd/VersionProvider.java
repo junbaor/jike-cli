9217e1f8d8d75a132b2fdd1329247ee2cb3c3c6c
junbaor <junbaor@gmail.com>
2021-11-29 10:45:03 +0800

    ✨ -v 命令添加 java 信息

--- src/main/java/com/github/junbaor/jike/cmd/VersionProvider.java
+++ src/main/java/com/github/junbaor/jike/cmd/VersionProvider.java
@@ -6,7 +6,20 @@ public class VersionProvider implements CommandLine.IVersionProvider {
 
     @Override
     public String[] getVersion() throws Exception {
-        return new String[]{"v0.1.3"};
+        String javaVmName = System.getProperty("java.vm.name");
+        String javaVersion = System.getProperty("java.version");
+        String javaHome = System.getProperty("java.home");
+
+        StringBuilder javaInfo = new StringBuilder();
+        javaInfo.append("Java version").append(": ").append(javaVersion).append(", ");
+        javaInfo.append("VM name").append(": ").append(javaVmName).append(", ");
+        javaInfo.append("runtime").append(": ").append(javaHome);
+
+        String jikeCliVersion = "v0.1.3";
+        return new String[]{
+                "jike-cli " + jikeCliVersion,
+                javaInfo.toString()
+        };
     }
 
 }
